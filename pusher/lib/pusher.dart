a7a1e5e0e11c82cf93fb1f41e064d718c9fb1a7d
Jason Rai <jrai@homex.com>
2019-05-21 16:10:30 -0400

    started scaffolding out API based on java docs

--- pusher/lib/pusher.dart
+++ pusher/lib/pusher.dart
@@ -1,13 +1,50 @@
-import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class Pusher {
-  static const MethodChannel _channel =
-      const MethodChannel('pusher');
+  Pusher._();
 
+  static const MethodChannel _channel = const MethodChannel('pusher');
+  static Map<String, Channel> _subscriptions = Map<String, Channel>();
+
+  /// Your pusher.com app key
+  static String appKey;
+
+  /// Pusher options. Example: the cluster to connect to
+  static PusherOptions options;
+
+  //todo remove
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  /// Connect the client to pusher
+  static Future connect(
+      {Function(ConnectionStateChange) onConnectionStateChange,
+      Function(ConnectionError) onError}) async {}
+
+  /// Disconnect the client from pusher
+  static Future disconnect() async {}
+
+  /// Subscribe to a channel
+  /// Use the returned [Channel] to bind events
+  static Future<Channel> subscribe(String channelName) async {}
+}
+
+class PusherOptions {
+  String cluster;
+}
+
+class ConnectionStateChange {}
+
+class ConnectionError {}
+
+class Event {
+  String channel;
+  String event;
+  String data;
+}
+
+class Channel {
+  void bind(String eventName, Function(Event) onEvent) {}
 }
