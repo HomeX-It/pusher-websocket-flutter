00da8d6a37c4e4c259b8ef034556abcea2f6fa4b
Jason Rai <jrai@homex.com>
2019-05-24 11:47:03 -0400

    started on iOS

--- pusher/ios/Classes/SwiftPusherPlugin.swift
+++ pusher/ios/Classes/SwiftPusherPlugin.swift
@@ -1,14 +1,26 @@
 import Flutter
 import UIKit
+import PusherSwift
 
 public class SwiftPusherPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "pusher", binaryMessenger: registrar.messenger())
-    let instance = SwiftPusherPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "pusher", binaryMessenger: registrar.messenger())
+        let instance = SwiftPusherPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "init":
+            setup(call, result: result)
+        default:
+            return
+        }
+        
+        
+        let pusher = Pusher(key: "APP_KEY")
+    }
+    
+    public func setup(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+    }
 }
--- pusher/ios/pusher.podspec
+++ pusher/ios/pusher.podspec
@@ -6,16 +6,17 @@ Pod::Spec.new do |s|
   s.version          = '0.0.1'
   s.summary          = 'A new flutter plugin project.'
   s.description      = <<-DESC
-A new flutter plugin project.
+Pusher Flutter Client
                        DESC
-  s.homepage         = 'http://example.com'
-  s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.homepage         = 'https://github.com/HomeXLabs/pusher-websocket-flutter'
+  s.license          = { :file => '../../LICENSE' }
+  s.author           = { 'HomeX' => 'jrai@homex.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+  s.dependency 'PusherSwift', '7.0.0'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '9.0'
 end
 
