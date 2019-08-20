321ca1f2d765ec757b3c4a1f4448c6bbee048074
thsowers <tyler@startrakstudios.com>
2019-08-20 14:07:51 -0400

    bump json annotation & json serializable packages

--- lib/pusher.g.dart
+++ lib/pusher.g.dart
@@ -8,92 +8,102 @@ part of 'pusher.dart';
 
 InitArgs _$InitArgsFromJson(Map<String, dynamic> json) {
   return InitArgs(
-      json['appKey'] as String,
-      json['options'] == null
-          ? null
-          : PusherOptions.fromJson(json['options'] as Map<String, dynamic>),
-      isLoggingEnabled: json['isLoggingEnabled'] as bool);
+    json['appKey'] as String,
+    json['options'] == null
+        ? null
+        : PusherOptions.fromJson(json['options'] as Map<String, dynamic>),
+    isLoggingEnabled: json['isLoggingEnabled'] as bool,
+  );
 }
 
 Map<String, dynamic> _$InitArgsToJson(InitArgs instance) => <String, dynamic>{
       'appKey': instance.appKey,
       'options': instance.options,
-      'isLoggingEnabled': instance.isLoggingEnabled
+      'isLoggingEnabled': instance.isLoggingEnabled,
     };
 
 BindArgs _$BindArgsFromJson(Map<String, dynamic> json) {
   return BindArgs(
-      channelName: json['channelName'] as String,
-      eventName: json['eventName'] as String);
+    channelName: json['channelName'] as String,
+    eventName: json['eventName'] as String,
+  );
 }
 
 Map<String, dynamic> _$BindArgsToJson(BindArgs instance) => <String, dynamic>{
       'channelName': instance.channelName,
-      'eventName': instance.eventName
+      'eventName': instance.eventName,
     };
 
 PusherOptions _$PusherOptionsFromJson(Map<String, dynamic> json) {
-  return PusherOptions(cluster: json['cluster'] as String);
+  return PusherOptions(
+    cluster: json['cluster'] as String,
+  );
 }
 
 Map<String, dynamic> _$PusherOptionsToJson(PusherOptions instance) =>
-    <String, dynamic>{'cluster': instance.cluster};
+    <String, dynamic>{
+      'cluster': instance.cluster,
+    };
 
 ConnectionStateChange _$ConnectionStateChangeFromJson(
     Map<String, dynamic> json) {
   return ConnectionStateChange(
-      currentState: json['currentState'] as String,
-      previousState: json['previousState'] as String);
+    currentState: json['currentState'] as String,
+    previousState: json['previousState'] as String,
+  );
 }
 
 Map<String, dynamic> _$ConnectionStateChangeToJson(
         ConnectionStateChange instance) =>
     <String, dynamic>{
       'currentState': instance.currentState,
-      'previousState': instance.previousState
+      'previousState': instance.previousState,
     };
 
 ConnectionError _$ConnectionErrorFromJson(Map<String, dynamic> json) {
   return ConnectionError(
-      message: json['message'] as String,
-      code: json['code'] as String,
-      exception: json['exception'] as String);
+    message: json['message'] as String,
+    code: json['code'] as String,
+    exception: json['exception'] as String,
+  );
 }
 
 Map<String, dynamic> _$ConnectionErrorToJson(ConnectionError instance) =>
     <String, dynamic>{
       'message': instance.message,
       'code': instance.code,
-      'exception': instance.exception
+      'exception': instance.exception,
     };
 
 Event _$EventFromJson(Map<String, dynamic> json) {
   return Event(
-      channel: json['channel'] as String,
-      event: json['event'] as String,
-      data: json['data'] as String);
+    channel: json['channel'] as String,
+    event: json['event'] as String,
+    data: json['data'] as String,
+  );
 }
 
 Map<String, dynamic> _$EventToJson(Event instance) => <String, dynamic>{
       'channel': instance.channel,
       'event': instance.event,
-      'data': instance.data
+      'data': instance.data,
     };
 
 PusherEventStreamMessage _$PusherEventStreamMessageFromJson(
     Map<String, dynamic> json) {
   return PusherEventStreamMessage(
-      event: json['event'] == null
-          ? null
-          : Event.fromJson(json['event'] as Map<String, dynamic>),
-      connectionStateChange: json['connectionStateChange'] == null
-          ? null
-          : ConnectionStateChange.fromJson(
-              json['connectionStateChange'] as Map<String, dynamic>),
-      connectionError: json['connectionError'] == null
-          ? null
-          : ConnectionError.fromJson(
-              json['connectionError'] as Map<String, dynamic>));
+    event: json['event'] == null
+        ? null
+        : Event.fromJson(json['event'] as Map<String, dynamic>),
+    connectionStateChange: json['connectionStateChange'] == null
+        ? null
+        : ConnectionStateChange.fromJson(
+            json['connectionStateChange'] as Map<String, dynamic>),
+    connectionError: json['connectionError'] == null
+        ? null
+        : ConnectionError.fromJson(
+            json['connectionError'] as Map<String, dynamic>),
+  );
 }
 
 Map<String, dynamic> _$PusherEventStreamMessageToJson(
@@ -101,5 +111,5 @@ Map<String, dynamic> _$PusherEventStreamMessageToJson(
     <String, dynamic>{
       'event': instance.event,
       'connectionStateChange': instance.connectionStateChange,
-      'connectionError': instance.connectionError
+      'connectionError': instance.connectionError,
     };
