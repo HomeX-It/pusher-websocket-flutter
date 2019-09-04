ceec4ea41ec7c254780afdbc6cc36edae51e75f7
Jason Rai <jrai@homex.com>
2019-09-04 08:24:40 -0400

    Add iOS ObjC based example and docs for setting up ObjC based Flutter apps (#8)
    * add example objc project
    
    * use local
    
    * remove test
    
    * add swift version, remove static_framework
    
    * make objc example work
    
    set global platform to 9, add use_frameworks!, set SWIFT_VERSION
    
    * doc and version update

---
+++ example_objc/lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pusher_websocket_flutter/pusher.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Event lastEvent;
+  String lastConnectionState;
+  Channel channel;
+
+  var channelController = TextEditingController(text: "my-channel");
+  var eventController = TextEditingController(text: "my-event");
+
+  @override
+  void initState() {
+    super.initState();
+    initPusher();
+  }
+
+  Future<void> initPusher() async {
+    try {
+      await Pusher.init("APP_KEY", PusherOptions(cluster: "us2"), enableLogging: true);
+    } on PlatformException catch (e) {
+      print(e.message);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Plugin example app'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _buildInfo(),
+                RaisedButton(
+                  child: Text("Connect"),
+                  onPressed: () {
+                    Pusher.connect(onConnectionStateChange: (x) async {
+                      if (mounted)
+                        setState(() {
+                          lastConnectionState = x.currentState;
+                        });
+                    }, onError: (x) {
+                      debugPrint("Error: ${x.message}");
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Disconnect"),
+                  onPressed: () {
+                    Pusher.disconnect();
+                  },
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        controller: channelController,
+                        decoration: InputDecoration(hintText: "Channel"),
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text("Subscribe"),
+                      onPressed: () async {
+                        channel = await Pusher.subscribe(channelController.text);
+                      },
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        controller: channelController,
+                        decoration: InputDecoration(hintText: "Channel"),
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text("Unsubscribe"),
+                      onPressed: () async {
+                        await Pusher.unsubscribe(channelController.text);
+                        channel = null;
+                      },
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        controller: eventController,
+                        decoration: InputDecoration(hintText: "Event"),
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text("Bind"),
+                      onPressed: () async {
+                        await channel.bind(eventController.text, (x) {
+                          if (mounted)
+                            setState(() {
+                              lastEvent = x;
+                            });
+                        });
+                      },
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        controller: eventController,
+                        decoration: InputDecoration(hintText: "Event"),
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text("Unbind"),
+                      onPressed: () async {
+                        await channel.unbind(eventController.text);
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+
+  Widget _buildInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Connection State: ",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(lastConnectionState ?? "Unknown"),
+          ],
+        ),
+        SizedBox(height: 8),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Last Event Channel: ",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(lastEvent?.channel ?? ""),
+          ],
+        ),
+        SizedBox(height: 8),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Last Event Name: ",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(lastEvent?.event ?? ""),
+          ],
+        ),
+        SizedBox(height: 8),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Last Event Data: ",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(lastEvent?.data ?? ""),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- ios/pusher_websocket_flutter.podspec
+++ ios/pusher_websocket_flutter.podspec
@@ -3,7 +3,7 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'pusher_websocket_flutter'
-  s.version          = '0.0.8'
+  s.version          = '0.1.1'
   s.summary          = 'A new flutter plugin project.'
   s.description      = <<-DESC
 Pusher Flutter Client
@@ -14,9 +14,9 @@ Pusher Flutter Client
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
+  s.swift_version = '4.2'
   s.dependency 'Flutter'
   s.dependency 'PusherSwift', '7.0.0'
-  s.static_framework = true
 
   s.ios.deployment_target = '9.0'
 end
